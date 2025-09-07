64b24e6afdeecc72e8099a35f2c79bb78e9af146
AnilKumar042 <62852264+AnilKumar042@users.noreply.github.com>
2025-09-07 16:12:54 -0500

    Project added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <div className="card">
+        Welcome to My application
+      </div>
+    </>
+  )
+}
+
+export default App
